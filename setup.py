efb20ae9efe325b855338fd06de0c53a9dbe1dd9
ArnaudCassan <cassan@iap.fr>
2017-09-29 10:34:37 +0200

    Update setup files

---
+++ setup.py
@@ -0,0 +1,58 @@
+name = 'microlensing'
+
+import sys
+import os
+
+from distutils.core import setup
+
+pjoin = os.path.join
+here = os.path.abspath(os.path.dirname(__file__))
+
+packages = []
+for d, _, _ in os.walk(pjoin(here, name)):
+    if os.path.exists(pjoin(d, '__init__.py')):
+        packages.append(d[len(here)+1:].replace(os.path.sep, '.'))
+
+version_ns = {}
+with open(pjoin(here, name, '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
+
+setup_args = dict(
+    name            = name,
+    version         = version_ns['__version__'],
+    packages        = packages,
+    description     = "Python modelling suite for gravitational microlensing",
+    long_description= "Python modelling suite for gravitational microlensing",
+    author          = 'Arnaud Cassan',
+    author_email    = 'cassan@iap.fr',
+    url             = 'https://github.com/ArnaudCassan/microlensing',
+    license         = 'Apache',
+    platforms       = "Linux, Mac OS X, Windows",
+    keywords        = ['Astronomy', 'Astrophysics', 'Microlensing', 'Science', 'exoplanets'],
+    classifiers     = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+
+if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
+    import setuptools
+
+setuptools_args = {}
+
+install_requires = setuptools_args['install_requires'] = [
+    'numpy'
+]
+
+extras_require = setuptools_args['extras_require'] = {
+}
+
+if 'setuptools' in sys.modules:
+    setup_args.update(setuptools_args)
+
+if __name__ == '__main__':
+    setup(**setup_args)
